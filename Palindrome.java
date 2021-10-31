59f9b3fb4717c112b8a25f9de58afe7d5a8f5a7e
Kalpesh Patil <76467196+kalpeshpatil811@users.noreply.github.com>
2021-10-31 22:15:29 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,24 +1,23 @@
-package Edureka;
 import java.util.Scanner;
-public class Palindrome {
-static void checkPalindrome(String input) {
-//Assuming result to be true
-boolean res = true;
-int length = input.length();
-//dividing the length of the string by 2 and comparing it.
-for(int i=0; i<= length/2; i++) {
-if(input.charAt(i) != input.charAt(length-i-1)) {
-res = false;
-break;
-}
-}
-System.out.println(input + " is palindrome = "+res);
-}
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-System.out.print("Enter your Statement: ");
-String str = sc.nextLine();
-//function call
-checkPalindrome(str);
-}
+
+class Palindrome
+{
+	public static void main(String arg[])
+	{
+		String name;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter Name:");
+		name=sc.nextLine();
+		StringBuffer name2=new StringBuffer(name);
+		name2.reverse();
+		if(name.equalsIgnoreCase(name2.toString()))
+		{
+			System.out.println("Palindrome");
+		}
+		else
+		{
+			System.out.println("Not Palindrome");
+		}
+	}
 }
