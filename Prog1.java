5803d3dad95bb956e710d8abacf481b8262a2ec0
Kalpesh Patil <76467196+kalpeshpatil811@users.noreply.github.com>
2021-10-31 22:24:41 +0530

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,11 @@
+class Prog1
+{
+	public static void main(String arg[])
+	{
+		int a=20;
+		double b=34.6;
+		
+		System.out.println("Heloo All");	
+		System.out.println("Integer Value="+a+"\nDouble Value="+b);
+	}
+}
