fefd222be6b5c4051e4908901f0b4a4a7d60ae88
Kalpesh Patil <76467196+kalpeshpatil811@users.noreply.github.com>
2021-10-31 22:25:40 +0530

    Create Prog4.java

---
+++ Prog4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Prog3
+{
+	public static void main(String arg[])
+	{
+		int a;
+		String name;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Name:");
+		name=sc.nextLine();
+		System.out.println("Name Of Student Is "+name);
+		System.out.println("Enter Integer Value:");
+		a=sc.nextInt();
+		System.out.println("Value = "+a);
+	}
+}
